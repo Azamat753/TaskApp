4e15df668869e363e9ef92930bfeae6570748dec
azamat753 <azamat.nazar99@gmail.com>
2019-11-30 15:47:45 +0600

    Initial commit

---
+++ app/src/main/java/com/geekteck/taskapp/FormActivity.java
@@ -0,0 +1,36 @@
+package com.geekteck.taskapp;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.geekteck.taskapp.ui.home.HomeFragment;
+
+public class FormActivity extends AppCompatActivity {
+     private EditText editTitle;
+     private EditText editdescription;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+        editTitle= findViewById(R.id.editTitle);
+        editdescription= findViewById(R.id.editDescription);
+    }
+
+    public void OnClick(View view) {
+        String title = editTitle.getText().toString().trim();
+        String description= editdescription.getText().toString().trim();
+
+        Intent intent  = new Intent();
+        intent.putExtra("title",title);
+        intent.putExtra("description",description);
+        setResult(RESULT_OK,intent);
+        finish();
+    }
+
+}
---
+++ app/src/main/java/com/geekteck/taskapp/ui/home/HomeFragment.java
@@ -0,0 +1,59 @@
+package com.geekteck.taskapp.ui.home;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.geekteck.taskapp.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeFragment extends Fragment {
+    private TaskAdapter adapter;
+
+    private List<Task> list;
+
+    private HomeViewModel homeViewModel;
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        RecyclerView recyclerView = root.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.addItemDecoration(new DividerItemDecoration(getContext(),DividerItemDecoration.VERTICAL));
+
+
+
+        list = new ArrayList<>();
+        list.add(new Task("Хасан", " "));
+
+        adapter = new TaskAdapter(list);
+        recyclerView.setAdapter(adapter);
+        return root;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode== Activity.RESULT_OK&&requestCode==1){
+            String title = data.getStringExtra("title");
+            String desc = data.getStringExtra("description");
+
+            list.add(new Task(title,desc));
+        }
+
+    }
+}
